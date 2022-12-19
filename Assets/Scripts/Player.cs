753d2793a840eaa81113c35cfdb408ee185b706a
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2022-12-19 15:05:29 -0500

    Player inheritance

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D playerBody;
+    public float horizontalMoveSpeed;
+    public float verticalMoveSpeed;
+    public KeyCode upInput;
+    public KeyCode leftInput;
+    public KeyCode rightInput;
+    
+
+    public void playerMovement()
+    {
+        bool isUpInput = Input.GetKey(upInput);
+        bool isLeftInput = Input.GetKey(leftInput);
+        bool isRightInput = Input.GetKey(rightInput);
+
+        if(isUpInput)
+        {
+            if (playerBody.velocity.y <= 0.01 && playerBody.velocity.y >= -0.01)
+                 playerBody.velocity = new Vector2(playerBody.velocity.x, verticalMoveSpeed);
+        }
+
+        if (isRightInput) 
+            playerBody.velocity = new Vector2(horizontalMoveSpeed, playerBody.velocity.y);
+
+        if (isLeftInput)
+            playerBody.velocity = new Vector2(-horizontalMoveSpeed, playerBody.velocity.y);
+
+    }
+}
