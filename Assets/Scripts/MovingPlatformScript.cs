598d716791c56b60fcef35fa08952a4aba05e9b6
Nic7082 <nicholasjennings708@yahoo.com>
2023-01-01 15:14:31 -0500

    Update MovingPlatformScript.cs

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -6,18 +6,42 @@ using UnityEngine.SocialPlatforms;
 
 public class MovingPlatformScript : MonoBehaviour
 {
+    //Data Fields
     public GameObject platformPathStart;
     public GameObject platformPathEnd;
     public int speed;
     private Vector3 startPosition;
     private Vector3 endPosition;
-  
-    // Update is called once per frame
+    private GameObject target = null;
+    private Vector3 offset;
+    
+    //Methods
+    void Start()
+    {
+        target = null;
+    }
+    
     void Update()
     {
         startPosition = platformPathStart.transform.position;
         endPosition = platformPathEnd.transform.position;
         float timeScale = speed / Vector3.Distance(startPosition, endPosition);
         gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, Mathf.Abs(Time.time * timeScale % 2 - 1));
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        target = col.gameObject;
+        offset = target.transform.position - transform.position;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        target = null;
+    }
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            target.transform.position = transform.position + offset;
+        }
     }
 }
