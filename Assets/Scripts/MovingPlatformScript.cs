a597cb6e9f75c91c10920b5187c6f8d4050a16e9
Nic7082 <nicholasjennings708@yahoo.com>
2023-01-23 21:56:15 -0500

    Revert "Update MovingPlatformScript.cs"
    This reverts commit 00269daaabf373a05f0b8158b58dd8f16df82144.

--- Assets/Scripts/MovingPlatformScript.cs
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -12,12 +12,36 @@ public class MovingPlatformScript : MonoBehaviour
     public int speed;
     private Vector3 startPosition;
     private Vector3 endPosition;
-
+    private GameObject target = null;
+    private Vector3 offset;
+    
     //Methods
+    void Start()
+    {
+        target = null;
+    }
+    
     void Update()
     {
         startPosition = platformPathStart.transform.position;
+        endPosition = platformPathEnd.transform.position;
         float timeScale = speed / Vector3.Distance(startPosition, endPosition);
-        gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, Mathf.Abs(Time.time* timeScale % 2 - 1));
+        gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, Mathf.Abs(Time.time * timeScale % 2 - 1));
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        target = col.gameObject;
+        offset = target.transform.position - transform.position;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        target = null;
+    }
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            target.transform.position = transform.position + offset;
+        }
     }
 }
